257f046d3775838f47c9bd725b29e7317c63fb5d
dev03lkomavyax <dev03lko@mavyax.com>
2025-09-03 17:43:19 +0530

    refactor: comment out RecommendedCourses import and usage in MyCourses component

--- src/app/(main)/user/my-courses/page.jsx
+++ src/app/(main)/user/my-courses/page.jsx
@@ -1,9 +1,8 @@
 import { FreshJobs } from '@/components/home/fresh-jobs/fresh-jobs'
 import { CompletedCourses } from '@/components/user-profile/completed-course/completed-course'
 import { MyActiveCourses } from '@/components/user-profile/my-active-courses/my-active-courses'
-import { RecommendedCourses } from '@/components/user-profile/recommended-courses/recommended-courses'
+// import { RecommendedCourses } from '@/components/user-profile/recommended-courses/recommended-courses'
 import { WelcomeBack } from '@/components/user-profile/welcome-back/welcome-back'
-import React from 'react'
 
 const MyCourses = () => {
   return (
@@ -12,7 +11,7 @@ const MyCourses = () => {
       <MyActiveCourses />
       <FreshJobs className="pt-12 md:pt-24" />
       <CompletedCourses />
-      <RecommendedCourses />
+      {/* <RecommendedCourses /> */}
     </section>
   )
 }
